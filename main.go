e55f9d69ced049cb025c0ffcf7eff3a4ff3127bd
Max Zhao <interarticle@gmail.com>
2017-06-01 00:05:57 -0700

    Prometheus metrics recording.

--- main.go
+++ main.go
@@ -1,92 +1,82 @@
 package main
 
 import (
-	"encoding/gob"
 	"flag"
-	"github.com/google/gopacket"
-	"github.com/google/gopacket/layers"
-	"github.com/google/gopacket/pcap"
-	"github.com/interarticle/bandwidth_recorder/data"
 	"log"
-	"os"
+	"net/http"
 	"sync/atomic"
 	"time"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/pcap"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-var device = flag.String("device", "eth0", "Name of the device to monitor.")
-var outputFile = flag.String("output_file", "", "gob file to write to.")
+var wanDevice = flag.String("wan_device", "eth0", "Name of the WAN (Internet) device to monitor.")
+var listenSpec = flag.String("listen_spec", "", "Host and port on which to provide Prometheus monitoring.")
 
-func main() {
-	flag.Parse()
-	if *outputFile == "" {
-		log.Fatal("you must specify --output_file")
-	}
+var (
+	wanTotalBytesGauge = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "wan_total_bytes",
+			Help: "Total number of bytes sent and received from the Internet as recorded by this recorder job instance",
+		}, []string{
+			"job_start_time",
+		})
+)
 
-	outputFile, err := os.OpenFile(*outputFile, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
+func networkMonitoringWorker() error {
+	startTime := time.Now()
+	jobBaseLabel := prometheus.Labels{"job_start_time": startTime.Format(time.RFC3339)}
+	gauge := wanTotalBytesGauge.With(jobBaseLabel)
+
+	log.Printf("Starting bandwidth monitoring on wanDevice %s", *wanDevice)
+	handle, err := pcap.OpenLive(*wanDevice, 500, false, pcap.BlockForever)
 	if err != nil {
-		log.Fatalf("Failed to open file: %v", err)
-	}
-	writer := gob.NewEncoder(outputFile)
-
-	log.Printf("Starting bandwidth monitoring on device %s", *device)
-	handle, err := pcap.OpenLive(*device, 500, false, pcap.BlockForever)
-	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	packets := gopacket.NewPacketSource(handle, handle.LinkType())
-	var packetCount uint64 = 0
-	var byteCount uint64 = 0
+	var layer2PlusTotal uint64
 	go func() {
 		for {
 			time.Sleep(time.Second)
-			log.Printf("%d packets/s; %d bytes/s", atomic.SwapUint64(&packetCount, 0), atomic.SwapUint64(&byteCount, 0))
+			gauge.Set(float64(atomic.LoadUint64(&layer2PlusTotal)))
 		}
 	}()
-	for packet := range packets.Packets() {
-		atomic.AddUint64(&packetCount, 1)
-		atomic.AddUint64(&byteCount, uint64(packet.Metadata().Length))
-		metadata := data.PacketMetadata{}
-		metadata.CaptureTime = packet.Metadata().Timestamp
-		metadata.TotalSize = packet.Metadata().Length
+	for {
+		packet, err := packets.NextPacket()
+		if err != nil {
+			return err
+		}
 		for i, layer := range packet.Layers() {
 			if _, ok := layer.(gopacket.ErrorLayer); ok {
 				continue // Ignore error layer.
 			}
 			switch i {
 			case 0:
-				metadata.Layer1Type = layer.LayerType()
-				metadata.Layer1Size = len(layer.LayerContents())
-				if eth, ok := layer.(*layers.Ethernet); ok {
-					metadata.SrcMAC = eth.SrcMAC
-					metadata.DstMAC = eth.DstMAC
-				}
-			case 1:
-				metadata.Layer2Type = layer.LayerType()
-				metadata.Layer2Size = len(layer.LayerContents())
-				if ip, ok := layer.(*layers.IPv4); ok {
-					metadata.SrcIP = ip.SrcIP
-					metadata.DstIP = ip.DstIP
-				} else if ip, ok := layer.(*layers.IPv6); ok {
-					metadata.SrcIP = ip.SrcIP
-					metadata.DstIP = ip.DstIP
-				}
-			case 2:
-				metadata.Layer3Type = layer.LayerType()
-				metadata.Layer3Size = len(layer.LayerContents())
-				if transport, ok := layer.(*layers.TCP); ok {
-					metadata.SrcPort = uint16(transport.SrcPort)
-					metadata.DstPort = uint16(transport.DstPort)
-				} else if transport, ok := layer.(*layers.UDP); ok {
-					metadata.SrcPort = uint16(transport.SrcPort)
-					metadata.DstPort = uint16(transport.DstPort)
-				} else if transport, ok := layer.(*layers.SCTP); ok {
-					metadata.SrcPort = uint16(transport.SrcPort)
-					metadata.DstPort = uint16(transport.DstPort)
-				}
+				atomic.AddUint64(&layer2PlusTotal, uint64(packet.Metadata().Length-len(layer.LayerContents())))
 			default:
-				break // Go up to three layers.
+				break // Stop at the first unmatched layer.
 			}
 		}
-		writer.Encode(metadata)
 	}
+}
+
+func init() {
+	prometheus.MustRegister(wanTotalBytesGauge)
+}
+
+func main() {
+	flag.Parse()
+
+	http.Handle("/metrics", promhttp.Handler())
+
+	go func() {
+		err := networkMonitoringWorker()
+		log.Fatal(err)
+	}()
+
+	log.Fatal(http.ListenAndServe(*listenSpec, nil))
 }
